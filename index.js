05cfe4dce6d2bfaf95f58e548ca0b213a1c924d6
eliran650 <34608563+eliran650@users.noreply.github.com>
2023-06-15 11:42:04 +0300

    test

---
+++ index.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+let response = null;
+new Promise(async (resolve, reject) => {
+  try {
+    response = await axios.get('https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
+      headers: {
+        'X-CMC_PRO_API_KEY': 'c1738fbd-f85d-43f4-8d92-5c7318da7cfb',
+      },
+    });
+  } catch(ex) {
+    response = null;
+    // error
+    console.log(ex);
+    reject(ex);
+  }
+  if (response) {
+    // success
+    const json = response.data;
+    console.log(json);
+    resolve(json);
+  }
+});
