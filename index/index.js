bfc17a54786a378cf13584ddea87f23e9c521473
eliran650 <eliranovadia7@gmail.com>
2023-06-23 10:51:25 +0300

    a

--- index/index.js
+++ index/index.js
@@ -2,11 +2,16 @@ var btc = document.getElementById("bitcoin");
 var ltc = document.getElementById("litecoin");
 var eth = document.getElementById("ethereum");
 var doge = document.getElementById("dogecoin");
+var bnb = document.getElementById("bnb");
+var ada = document.getElementById("cardano");
+var sol = document.getElementById("solana");
+var xrp = document.getElementById("xrp");
+var shib = document.getElementById("shibainu");
 
 var liveprice = {
     "async": true,
     "scroosDomain": true,
-    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Clitecoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Clitecoin%2Cethereum%2Cdogecoin%2Cbinancecoin%2Cxrp%2Ccardano%2Cavalanche&vs_currencies=usd",
 
     "method": "GET",
     "headers": {}
@@ -17,5 +22,9 @@ $.ajax(liveprice).done(function (response){
     ltc.innerHTML = response.litecoin.usd;
     eth.innerHTML = response.ethereum.usd;
     doge.innerHTML = response.dogecoin.usd;
-
+    bnb.innerHTML = response.binancecoin.usd;
+    xrp.innerHTML = response.xrp.usd;
+    ada.innerHTML = response.cardano.usd;
+    sol.innerHTML = response.solana.usd;
+    shib.innerHTML = response["shiba-inu"].usd;
 });
